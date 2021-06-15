3397b9bea2617b6efe293804c464fd569221d2a9
zhxl <zhxl@njzdgt.com>
2021-06-15 14:23:25 +0800

    test

---
+++ ticket/src/main/java/org/five/ticket/controller/TestController.java
@@ -0,0 +1,19 @@
+package org.five.ticket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ticket")
+public class TestController {
+
+    @GetMapping("/test")
+    public String hello()
+    {
+        return "hello";
+    }
+
+
+}
--- ticket/src/main/java/org/five/ticket/entity/ticketEntity.java
+++ ticket/src/main/java/org/five/ticket/entity/ticketEntity.java
@@ -3,6 +3,7 @@ package org.five.ticket.entity;
 public class ticketEntity {
     private Integer id;
     private String uuid;
+    private Integer displayId;
 
 
 }
